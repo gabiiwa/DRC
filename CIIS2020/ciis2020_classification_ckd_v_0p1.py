e6f3c1e02a69f4803d4ce3887207ca3d9c53e37f
Leonardo Goliatt <goliatt@surucucu>
2020-07-22 14:31:36 -0300

    [goliatt qua jul 22 14:31:36 -03 2020]

---
+++ CIIS2020/ciis2020_classification_ckd_v_0p1.py
@@ -0,0 +1,172 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-    
+import numpy as np
+import pandas as pd
+import pygmo as pg
+import pylab as pl
+
+from sklearn.model_selection import (GridSearchCV, KFold, cross_val_predict, 
+                                     TimeSeriesSplit, cross_val_score, 
+                                     LeaveOneOut, KFold, StratifiedKFold,
+                                     RandomizedSearchCV,
+                                     cross_val_predict,train_test_split)
+from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.metrics.regression import mean_squared_error, mean_absolute_error, median_absolute_error
+from sklearn.metrics.classification import accuracy_score, f1_score, precision_score
+from sklearn.decomposition import KernelPCA
+from sklearn.preprocessing import MinMaxScaler, PolynomialFeatures, MaxAbsScaler, Normalizer, StandardScaler, MaxAbsScaler, FunctionTransformer, QuantileTransformer
+from sklearn.pipeline import Pipeline
+
+from sklearn.svm import SVR, LinearSVR, SVC
+from sklearn.linear_model import ElasticNet, Ridge, PassiveAggressiveRegressor, LogisticRegression, BayesianRidge
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, VotingRegressor
+from sklearn.neural_network import MLPRegressor
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.kernel_ridge import KernelRidge
+from xgboost import  XGBRegressor
+from sklearn.naive_bayes import GaussianNB
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import (RBF, Matern, RationalQuadratic,
+                                              ExpSineSquared, DotProduct,
+                                              ConstantKernel)
+
+
+#import re  
+import os
+#from sklearn.gaussian_process import GaussianProcess
+#from catboost import Pool, CatBoostRegressor
+#from pyearth import Earth as MARS
+#from sklearn.ensemble import StackingRegressor
+#from sklearn.experimental import enable_hist_gradient_boosting
+#from sklearn.ensemble import HistGradientBoostingRegressor
+#from sklearn.kernel_approximation import RBFSampler,SkewedChi2Sampler
+
+#from util.ELM import  ELMRegressor, ELMRegressor
+#from util.MLP import MLPRegressor as MLPR
+#from util.RBFNN import RBFNNRegressor, RBFNN
+#from util.LSSVR import LSSVR
+
+from scipy import stats
+
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler, PolynomialFeatures, MaxAbsScaler, Normalizer, StandardScaler, MaxAbsScaler, FunctionTransformer, QuantileTransformer
+from sklearn.svm import SVC
+from xgboost import XGBClassifier
+
+
+from read_data import *
+from sklearn import metrics
+from utils.confusion_matrix_pretty_print import plot_confusion_matrix_from_data
+from sklearn.metrics import classification_report
+
+#%%----------------------------------------------------------------------------
+def RMSE(y, y_pred):
+    y, y_pred = np.array(y).ravel(), np.array(y_pred).ravel()
+    error = y -  y_pred    
+    return np.sqrt(np.mean(np.power(error, 2)))
+
+def MAPE(y_true, y_pred):    
+  y_true, y_pred = np.array(y_true).ravel(), np.array(y_pred).ravel()
+  return np.mean(np.abs(y_pred - y_true)/np.abs(y_true))*100
+  #return RMSE(y, y_pred)
+  
+#%%----------------------------------------------------------------------------
+
+#pd.options.display.float_format = '{:20,.3f}'.format
+pd.options.display.float_format = '{:.3f}'.format
+
+import warnings
+warnings.filterwarnings('ignore')
+
+import sys, getopt
+program_name = sys.argv[0]
+arguments = sys.argv[1:]
+count = len(arguments)
+
+#print ("This is the name of the script: ", program_name)
+#print ("Number of arguments: ", len(arguments))
+#print ("The arguments are: " , arguments)
+
+if len(arguments)>0:
+    if arguments[0]=='-r':
+        run0 = int(arguments[1])
+        n_runs = run0+1
+else:
+    run0, n_runs = 0,1
+
+#%%----------------------------------------------------------------------------   
+
+
+#%%----------------------------------------------------------------------------   
+basename='evo_ml_'
+
+from read_data import *
+datasets = [
+    read_data_drc_35(),
+]
+#%%----------------------------------------------------------------------------   
+pd.options.display.float_format = '{:.3f}'.format
+from scipy.stats import uniform
+
+pop_size    = 20
+max_iter    = 40
+n_splits    = 3
+scoring     = 'neg_mean_squared_error'
+scoring     = 'neg_root_mean_squared_error'
+for run in range(run0, n_runs):
+    random_seed=run+100
+    
+    for dataset in datasets:#[:1]:
+        dr=dataset['name'].replace(' ','_').replace("'","").lower()
+        path='./pkl_'+dr+'/'
+        os.system('mkdir  '+path)
+        
+        #for (target,y_train,y_test) in zip(dataset['target_names'], dataset['y_train'], dataset['y_test']):                        
+        for tk, tn in enumerate(dataset['target_names']):
+            print (tk, tn)
+            target                          = dataset['target_names'][tk]
+            y_train, y_test                 = dataset['y_train'][tk], dataset['y_test'][tk]
+            dataset_name, X_train, X_test   = dataset['name'], dataset['X_train'], dataset['X_test']
+            n_samples_train, n_features     = dataset['n_samples'], dataset['n_features']
+            task, normalize                 = dataset['task'], dataset['normalize']
+            n_samples_test                  = len(y_test)
+            np.random.seed(random_seed)
+
+            s=''+'\n'
+            s+='='*80+'\n'
+            s+='Dataset                    : '+dataset_name+' -- '+target+'\n'
+            s+='Number of training samples : '+str(n_samples_train) +'\n'
+            s+='Number of testing  samples : '+str(n_samples_test) +'\n'
+            s+='Number of features         : '+str(n_features)+'\n'
+            s+='Normalization              : '+str(normalize)+'\n'
+            s+='Task                       : '+str(dataset['task'])+'\n'
+            s+='Reference                  : '+str(dataset['reference'])+'\n'
+            s+='='*80
+            s+='\n'            
+            
+            print(s)            
+            
+            distributions = dict(C=uniform(loc=0, scale=1e4),
+                                 gamma=uniform(loc=0, scale=10),
+                                 #degree=uniform(loc=1, scale=5),
+                               )
+            
+            clf = RandomizedSearchCV(estimator=SVC(kernel='linear'), 
+                                     param_distributions=distributions, 
+                                     n_iter=100,
+                                     n_jobs=-1,
+                                     scoring=scoring,
+                                     random_state=random_seed)
+            
+            clf.fit(X_train, y_train)
+            
+            y_pred = clf.predict(X_test)
+           
+            columns = [str(i) for i in np.unique(y_test)]
+            plot_confusion_matrix_from_data(y_test, y_pred, columns, figsize=[4, 4],)
+            
+            print(classification_report(y_test, y_pred))
+#%%----------------------------------------------------------------------------   
---
+++ CIIS2020/read_data.py
@@ -0,0 +1,178 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*
+import sys
+#reload(sys)
+#sys.setdefaultencoding("utf-8")
+
+import numpy as np
+import pylab as pl
+import matplotlib.pyplot as pl
+import pandas as pd
+import os
+import sys
+import re
+from scipy import stats
+
+#%% 
+def transforma_estagio(valor):
+    if   valor == 'Estágio 1 - >= 90 ml':  return '1'#1
+    elif valor == 'Estágio 2 - 60-89 ml':  return '1'#2
+    elif valor == 'Estágio 3a - 45-59 ml': return '1'#3
+    elif valor == 'Estágio 3b - 30-44 ml': return '1'#4
+    elif valor == 'Estágio 4 - 15-29 ml':  return '2'#5
+    elif valor == 'Estágio 5 - < 15 ml':   return '2'#6
+
+def transforma_raca(valor):
+    if valor == 'Branca': return 0
+    elif valor == 'Preta': return 1
+    elif valor == 'Parda': return 2
+    elif valor == 'Amarela': return 3
+    elif valor == 'Indigena': return 4
+    
+    
+def read_data_drc_35(
+            filename='./data/Banco35exames.csv',
+        ):
+    #%%
+    filename='./data/Banco35exames.csv'
+    df= pd.read_csv(filename,  delimiter=',')
+    
+    df['ESTAGIOI - BIN'] = df['ESTAGIOI - EQ'].map(transforma_estagio)
+    df['ESTAGIOF - BIN'] = df['ESTAGIOF - EQ'].map(transforma_estagio)
+    
+    df['Raça'] = df['Raça'].map(transforma_raca)
+    
+    df['Codsexo'] = df['Codsexo'].replace('Masculino', 0)
+    df['Codsexo'] = df['Codsexo'].replace('Feminino', 1)
+    
+    target_names = ['ESTAGIOF - BIN']
+    y = df[target_names]
+
+    feature_names=[
+         'Idade',
+         'Raça',
+         'Codsexo',
+         'PAS_inicial',
+         'PAS_final',
+         'PAD_inicial',
+         'PAD_final',
+         'pesoi',
+         'pesof',
+         'HemoglobinaI',
+         'ColesterolTotalI',
+         'GlicemiadeJejumI',
+         'TrigliceridesI',
+         'PotassioI',
+         'ColesterolHDLI',
+         'UreiaI',
+         'TSHI',
+         'AcidoUricoI',
+         'HemoglobinaGlicadaI',
+         'TGPI',
+         'GlicemiadeJejumF',
+         'ColesterolTotalF',
+         'TrigliceridesF',
+         'ColesterolHDLF',
+         'HemoglobinaF',
+         'SodioSericoI',
+         'PotassioF',
+         'CKI',
+         'CalcioTotalI',
+         'VITAMINADI',
+         'HemoglobinaGlicadaF',
+         'ColesterolLDLI',
+         'UreiaF',
+         'Proteinuria24hsI',
+         'TGPF',
+         ]
+    
+    df=df[feature_names]
+    
+    idx=[ True,  True,  True, False, False, False, False,  True,  True,
+       False, False,  True, False,  True,  True, False,  True,  True,
+        True, False,  True, False, False,  True,  True,  True,  True,
+        True,  True,  True,  True, False, False,  True,  True, False,
+        True,  True, False,  True, False, False,  True,  True,  True,
+        True,  True, False,  True,  True,  True,  True,  True,  True,
+        True,  True,  True,  True, False,  True, False,  True,  True,
+       False,  True,  True, False, False, False,  True, False, False,
+        True, False,  True, False, False,  True,  True, False,  True,
+        True, False,  True,  True,  True,  True, False, False,  True,
+        True,  True,  True,  True,  True,  True,  True,  True, False,
+        True,  True,  True,  True,  True,  True,  True,  True,  True,
+        True,  True,  True,  True,  True,  True, False,  True,  True,
+        True,  True, False, False,  True,  True, False, False, False,
+       False,  True,  True,  True,  True, False, False,  True,  True,
+       False,  True,  True, False,  True,  True,  True,  True,  True,
+        True, False,  True,  True,  True, False,  True,  True,  True,
+        True, False, False, False,  True, False,  True, False,  True,
+       False,  True,  True, False,  True, False,  True, False,  True,
+        True,  True,  True,  True, False,  True, False, False, False,
+        True,  True,  True, False,  True,  True,  True,  True,  True,
+        True,  True, False, False,  True,  True,  True,  True,  True,
+        True,  True,  True,  True, False,  True,  True, False,  True,
+        True,  True, False, False,  True,  True, False,  True,  True,
+       False,  True,  True,  True,  True,  True,  True,  True, False,
+       False,  True,  True,  True,  True,  True, False,  True, False,
+        True, False,  True,  True, False,  True, False, False,  True,
+       False,  True, False,  True, False, False,  True,  True,  True,
+       False,  True, False,  True,  True, False,  True,  True,    True,
+        True,  True, False, False,  True, False,  True,  True,    True,
+        True, False,  True,  True,  True, False,  True, False,    True,
+        True, False,  True,  True,  True,  True, False,  True,    True,
+        True,  True,  True,  True,  True,  True,  True, False,    True,
+        True, False,  True,  True,  True,  True,  True,  True,  True,
+       False, False,  True,  True, False,  True, False, False, False,
+       False, False,  True,  True,  True, False,  True, False,  True,
+        True,  True,  True,  True,  True,  True,  True,  True,  True,
+        True, False, False, False, False, False,  True, False,  True,
+        True, False,  True,  True,  True,  True,  True,  True, False,
+        True,  True, False,  True,  True, False,  True,  True,  True,
+        True,  True, False,  True,  True,  True,  True,  True,  True,
+        True, False,  True,  True, False, False,  True,  True,  True,
+        True, False, False,  True,  True,  True,  True,  True, False,
+        True,  True,  True,  True,  True,  True, False,  True, False,
+        True,  True,  True,  True, False, False, False,  True,  True,
+       False,  True, False, False,  True, False,  True,  True]
+    
+    idn = [not i for i in idx]
+    X_train, y_train = df[idx], y[idx]
+    X_test, y_test  = df[idn], y[idn]
+    data_description = ['X_'+str(i+1) for i in range(df.shape[1])]
+    n_samples, n_features = X_train.shape
+    dataset=  {
+      'task'            : 'classification',
+      'name'            : 'DRC 35 features',
+      'feature_names'   : feature_names,
+      'target_names'    : target_names,
+      'n_samples'       : n_samples, 
+      'n_features'      : n_features,
+      'X_train'         : X_train.values,
+      'X_test'          : X_test.values,
+      'y_train'         : y_train.values.T,
+      'y_test'          : y_test.values.T,      
+      'targets'         : feature_names,
+      'true_labels'     : None,
+      'predicted_labels': None,
+      'descriptions'    : data_description,
+      'items'           : None,
+      'reference'       : "https://www.sciencedirect.com/science/article/pii/S2352914818302387",      
+      'normalize'       : 'MinMax',
+      }
+    #%% 
+    return dataset
+
+
+#%% 
+
+#%%-----------------------------------------------------------------------------
+if __name__ == "__main__":
+    datasets = [
+                read_data_drc_35(),                
+            ]
+    for D in datasets:
+        print('='*80+'\n'+D['name']+'\n'+'='*80)
+        print(D['reference'])
+        print( D['y_train'])
+        print('\n')
+#%%-----------------------------------------------------------------------------
